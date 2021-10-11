a0282a16ae530347d2bd3b52f6ffd1b7102c4885
kkugosu <hjs9402@gmail.com>
2021-10-11 22:29:43 +0900

    first commit

---
+++ my_model/DQN.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+from torchvision.transforms import ToTensor, Lambda
+from torch import nn
+
+
+class DQN(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(DQN, self).__init__()
+        # self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+            nn.ReLU()
+        )
+
+    def forward(self, input_element):
+        output = self.linear_relu_stack(input_element)
+        return output
+

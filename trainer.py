1720263ed33425264423d36a021d05ae1a6c2108
kkugosu <hjs9402@gmail.com>
2021-10-14 00:46:53 +0900

    pg buffer remodeling

--- trainer.py
+++ trainer.py
@@ -8,36 +8,49 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class Train:
-    def __init__(self, envname, dataloader, updmodel1, basemodel2):
-        self.converter = converter.Converter(envname)
-        self.dataloader = dataloader
-        self.updmodel1 = updmodel1
-        self.basemodel2 = basemodel2
+    def __init__(self, env_name, data_loader, model_name):
+        self.converter = converter.Converter(env_name)
+        self.data_loader = data_loader
+        self.model_name = model_name
 
-    def training(self, iteration, batchsize, optimizer):
+    def training(self, iteration, batch_size, *model, optimizer):
         GAMMA = 0.999
         i = 0
+        upd_model = None
+        base_model = None
+
+        if self.model_name == "DQN":
+            upd_model = model[0]
+            base_model = model[1]
+        elif self.model_name == "PG":
+            upd_model = model[0]
+        else:
+            print("model input error")
+
         while i < iteration:
             # print(i)
-            pre_observation, action, observation, reward = next(iter(self.dataloader))
-            action_idx = self.converter.act2index(action, batchsize).astype(np.int64)
+            pre_observation, action, observation, reward = next(iter(self.data_loader))
+            action_idx = self.converter.act2index(action, batch_size).astype(np.int64)
             action_idx = torch.from_numpy(action_idx).to(device).unsqueeze(axis=-1)
             pre_obs_to_cuda = torch.tensor(pre_observation, dtype=torch.float32).to(device)
-            state_action_values = torch.gather(self.updmodel1(pre_obs_to_cuda), 1, action_idx)
-
+            state_action_values = torch.gather(upd_model(pre_obs_to_cuda), 1, action_idx)
             obs_to_cuda = torch.tensor(observation, dtype=torch.float32).to(device)
             reward_to_cuda = torch.tensor(reward, dtype=torch.float32).to(device)
-
-            with torch.no_grad():
-                nextobs = self.basemodel2(obs_to_cuda)
-                expected_state_action_values = GAMMA * torch.argmax(nextobs, dim=1) + reward_to_cuda
-
             criterion = nn.MSELoss()
-            loss = criterion(state_action_values, expected_state_action_values.unsqueeze(axis=-1))
+
+            if self.model_name == "DQN":
+                with torch.no_grad():
+                    nextobs = base_model(obs_to_cuda)
+                    expected_state_action_values = GAMMA * torch.argmax(nextobs, dim=1) + reward_to_cuda
+                loss = criterion(state_action_values, expected_state_action_values.unsqueeze(axis=-1))
+            elif self.model_name == "PG":
+                print("wait")
+            else:
+                print("training error")
 
             optimizer.zero_grad()
             loss.backward()
-            for param in self.updmodel1.parameters():
+            for param in upd_model.parameters():
                 param.grad.data.clamp_(-1, 1)
             optimizer.step()
 
--- utils/buffer.py
+++ utils/buffer.py
@@ -27,8 +27,9 @@ class ManageMem:
         elif self.modelname == "PG":
             with torch.no_grad():
                 basedqn_action = self.model(pre_observation)
-            max_action = np.argmax(basedqn_action.cpu().numpy())
-            action = self.converter.index2act(max_action, 1)
+            my_action = torch.multinomial(basedqn_action, 1)
+            my_action.cpu().numpy()
+            action = self.converter.index2act(my_action, 1)
             return action
 
         else:
@@ -36,24 +37,62 @@ class ManageMem:
             return None
 
     def renewal_memory(self, renewal_capacity, dataset):
+        gamma = 0.999
+        temp_gamma = 0
         total_num = 0
         pause = 0
         while total_num < renewal_capacity - pause:
             pre_observation = self.env.reset()
             pre_observation = torch.tensor(pre_observation, device=device, dtype=torch.float32)
             t = 0
-            while t < renewal_capacity - total_num:
-                t = t + 1
+            temp_preobs = np.array([])
+            temp_action = np.array([])
+            temp_obs = np.array([])
+            temp_reward = np.array([])
+            while t < renewal_capacity - total_num: #if pg, gain accumulate
                 action = self._select_act(pre_observation)
                 observation, reward, done, info = self.env.step(action)
                 np_pre_observation = pre_observation.cpu().numpy()
-                dataset.push(np_pre_observation, action, observation, reward - np.float32(done))
+
+                if self.modelname == "DQN":
+                    dataset.push(np_pre_observation, action, observation, reward - np.float32(done))
+
+                elif self.modelname == "PG":
+                    if t == 0:
+                        temp_preobs = np.array([np_pre_observation])
+                        temp_action = np.array([action])
+                        temp_obs = np.array([observation])
+                        temp_reward = np.array([reward])
+                    else:
+                        temp_gamma = np.power(gamma, t)
+                        temp_preobs = np.append(temp_preobs, np.array([pre_observation]), axis=0)
+                        temp_action = np.append(temp_action, np.array([action]), axis=0)
+                        temp_obs = np.append(temp_obs, np.array([observation]), axis=0)
+                        temp_reward = np.append(temp_reward, np.array([reward*temp_gamma]), axis=0)
+                else:
+                    print("renewal_memory_error")
+
                 pre_observation = torch.tensor(observation, device=device, dtype=torch.float32)
+                t = t + 1
+
                 if done:
-                    # print("Episode finished after {} timesteps".format(t+1))
+                    temp_reward[t-1] = temp_reward[t-1] - np.float32(done)*temp_gamma
+                    # if episode done in 5 step, t is 5
+                    if self.modelname == "PG":
+                        pg_step = 0
+                        while pg_step < t:  # per step
+                            reward_step = pg_step
+                            gain = np.float32(0)
+                            while reward_step < t:
+                                gain = gain + temp_reward[reward_step]
+                                reward_step = reward_step + 1
+                            dataset.push(temp_preobs, temp_action, temp_obs, gain)
+                            pg_step = pg_step + 1
+                    # print("Episode finished after {} timesteps".format(t))
                     total_num += t
                     t = 0
                     break
+
             pause = t
         # print("load_memory_complete")
 

de8f52310b0b71634b2e77adb3956dcd9d382d0f
kkugosu <hjs9402@gmail.com>
2022-07-21 17:34:28 +0900

    cac

--- control/AC.py
+++ control/AC.py
@@ -1,50 +1,52 @@
 from control import BASE, policy
+import gym
 import torch
+import numpy as np
+import sys
+from torchvision.transforms import ToTensor, Lambda
+from torch import nn
 from NeuralNetwork import NN
 from utils import buffer
-from torch import nn
-
-
-import numpy as np
+import random
+import torch.onnx as onnx
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-GAMMA = 0.999
+GAMMA = 0.98
 
 
-class ACPolicy(BASE.BasePolicy):
-    def __init__(self, step_size, *args) -> None:
+class PGPolicy(BASE.BasePolicy):
+    def __init__(self, *args) -> None:
         super().__init__(*args)
-        self.step_size = step_size
         self.updatedPG = NN.SimpleNN(self.o_s, self.h_s, self.a_s).to(device)
         self.updatedDQN = NN.SimpleNN(self.o_s, self.h_s, self.a_s).to(device)
         self.baseDQN = NN.SimpleNN(self.o_s, self.h_s, self.a_s).to(device)
         self.baseDQN.eval()
-        self.policy = policy.Policy(self.cont, self.baseDQN, self.env_n)
-        self.buffer = buffer.Simulate(self.env, self.policy, step_size=4)
-        self.optimizer = torch.optim.SGD(self.updatedDQN.parameters(), lr=self.lr)
+        self.policy = policy.Policy(self.cont, self.updatedPG, self.env_n)
+        self.buffer = buffer.Simulate(self.env, self.policy, step_size=100)
+        self.optimizer = torch.optim.SGD(self.updatedPG.parameters(), lr=self.lr)
+        self.softmax = nn.Softmax(dim=-1)
 
-    def training(self):
-        i = 0
-        try:
-            self.updatedDQN.load_state_dict(torch.load(self.PARAM_PATH_TEST))
-            self.updatedPG.load_state_dict(torch.load(self.PARAM_PATH_TEST))
-        except:
+    def training(self, load=int(0)):
+
+        if int(load) == 1:
+            print("loading")
+            self.updatedPG.load_state_dict(torch.load(self.PARAM_PATH))
+            self.updatedDQN.load_state_dict(torch.load(self.PARAM_PATH))
+            print("loading complete")
+        else:
             pass
-        while i < self.t_i:
+        i = 0
+        while i < 10*self.t_i:
             i = i + 1
             self.buffer.renewal_memory(self.ca, self.data, self.dataloader)
-            loss = self.train_per_buf(self.t_i, self.b_s, self.optimizer, self.updatedDQN, self.baseDQN)
+            loss = self.train_per_buff(self.t_i, self.b_s, self.optimizer, self.updatedPG)
             self.writer.add_scalar("loss", loss, i)
-            torch.save(self.updatedDQN.state_dict(), self.PARAM_PATH_TEST)
-            self.baseDQN.load_state_dict(self.updatedDQN.state_dict())
-            self.baseDQN.eval()
+            torch.save(self.updatedPG.state_dict(), self.PARAM_PATH)
 
         self.env.close()
         self.writer.flush()
         self.writer.close()
 
-    def train_per_buf(self, iteration, batch_size, optimizer, *model):
-        upd_model = model[0]
-        base_model = model[1]
+    def train_per_buff(self, iteration, batch_size, optimizer, upd_model):
         i = 0
         while i < iteration:
             # print(i)
@@ -52,19 +54,26 @@ class ACPolicy(BASE.BasePolicy):
             n_a_index = self.converter.act2index(n_a, batch_size).astype(np.int64)
             t_a_index = torch.from_numpy(n_a_index).to(device).unsqueeze(axis=-1)
             t_p_o = torch.tensor(n_p_o, dtype=torch.float32).to(device)
-            t_p_qsa = torch.gather(upd_model(t_p_o), 1, t_a_index)
-            t_o = torch.tensor(n_o, dtype=torch.float32).to(device)
             t_r = torch.tensor(n_r, dtype=torch.float32).to(device)
-            criterion = nn.MSELoss()
+            print("trsh", np.shape(t_r))
+            t_p_o_softmax = self.softmax(upd_model(t_p_o))
+            state_action_values = torch.gather(upd_model(t_p_o_softmax), 1, t_a_index)
+            DQN_weight = torch.gather(self.updatedDQN(t_p_o), 1, t_a_index)
+            print("sa", np.shape(state_action_values))
+            weight = torch.log(state_action_values)
+            loss = -torch.matmul(weight, t_r)
+            # [1,2,3] * [1,2,3] = [1,4,9]
             with torch.no_grad():
-                t_p_qsa_ = base_model(t_o)
+                t_p_qsa_ = self.baseDQN(t_o)
                 t_p_qsa_ = torch.max(t_p_qsa_, dim=1)[0] * GAMMA
                 t_p_qsa_ = t_p_qsa_ + t_r
-            loss = criterion(t_p_qsa, t_p_qsa_.unsqueeze(axis=-1))
+            print("wait")
             optimizer.zero_grad()
             loss.backward()
             for param in upd_model.parameters():
                 param.grad.data.clamp_(-1, 1)
             optimizer.step()
             i = i + 1
+            print("loss = ", loss)
+
         return loss
